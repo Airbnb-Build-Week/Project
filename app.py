10455e281ae4c91f0e7246e11d937ec5d448061d
Mikayla Kosmala <53801463+Mikaykay@users.noreply.github.com>
2022-03-02 18:56:14 -0500

    flask app integration

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ def Home_page():
     '''Landing page to the Kickstarter Prediction project'''
     return render_template('landing.html', title='Home')
 
-@APP.route('/prediction', methods= ["POST"])
+@APP.route('/', methods= ["POST"])
 def prediction():
     lat = request.form['lat']
     long = request.form['long']
     room_type = request.form['room_type']
     accomodates = request.form['accomodates']
+    print(accomodates)
     #prj_length = request.form['length']
     # ks = create_project_df(prj_name, prj_desc, prj_goal, prj_category, prj_length)
     # predify = model.predict(ks)
@@ -29,6 +30,7 @@ def prediction():
     #     pred_result = 'an utter failure. Re-think your life, and may God have mercy on your soul.'
     # if predify == [1]:
     #     pred_result = 'a successful individual. Revel in your glory, and be kind as you stare down on those less fortunate.' 
-    # return render_template('prediction.html',
-    #                        title="Prediction",
-    #                        prediction=pred_result)
+
+    return render_template('landing.html',
+                       #prediction=pred_result
+                       )
