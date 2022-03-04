587cae64ef3a98b3b90f0e0691ac89c195fd4780
Mikayla Kosmala <53801463+Mikaykay@users.noreply.github.com>
2022-03-03 19:54:17 -0500

    debugging

--- app.py
+++ app.py
@@ -20,15 +20,15 @@ def prediction():
     lon = float(request.form['lon'])
     # Room Type, Superhost, Instant Bookable, Description Length
     room_type = request.form['room_type']
-    super_host = True if request.form['superhost']==1 else False
-    instant_bookable = True if request.form['instant_bookable']==1 else False
-    description_len = len(request.form['description_len'])
+    super_host = True if request.form['super_host']=='1' else False
+    instant_bookable = True if request.form['instant_bookable']=='1' else False
+    description_len = len(request.form['description'])
     # Accomodates, Bedrooms, Beds, Baths, Shared Baths, ppl_per_bed
     accommodates = int(request.form['accomodates']) #
     n_bedrooms = int(request.form['n_bedrooms']) #
     n_beds = int(request.form['n_beds']) #
     n_baths = int(request.form['n_baths']) #
-    shared_baths = True if request.form['shared_baths']==1 else False
+    shared_baths = True if request.form['shared_baths']=='1' else False
     ppl_per_bed = accommodates/n_beds if n_beds!=0 else accommodates/1
     n_amenities = int(request.form['n_amenities']) #
     # Host experience, total reviews, total_statisfaction, reviews since
@@ -38,11 +38,11 @@ def prediction():
     total_satisfaction = float(request.form['total_satisfaction']) # If we average the survey values then how are we going to ask 
     reviews_per_month = total_reviews/(host_experience_yrs*12)
     # Min and Max nights
-    min_nights = request.form['min_nights']
-    max_nights = request.form['max_nights']
+    min_nights = int(request.form['min_nights'])
+    max_nights = int(request.form['max_nights'])
 
     # Dataframe
-    column_names = ['lat', 'lon', 'room_type', 'superhost', 'instant_bookable',
+    column_names = ['lat', 'lon', 'room_type', 'super_host', 'instant_bookable',
        'description_len', 'n_amenities', 'accommodates', 'n_bedrooms',
        'n_beds', 'n_baths', 'shared_baths', 'min_nights', 'max_nights',
        'reviews_per_month', 'total_reviews', 'total_satisfaction',
@@ -53,19 +53,20 @@ def prediction():
        n_beds, n_baths, shared_baths, min_nights, max_nights,
        reviews_per_month, total_reviews, total_satisfaction,
        host_experience_yrs, ppl_per_bed]]
+
     listing = pd.DataFrame(info,columns=column_names)
     # # Choose what time of year it is
     # # Run correct model
-    if time_of_year == "Winter":
+    if time_of_year == "winter":
       model_winter = joblib.load("ModelWinter")
       pred = model_winter.predict(listing)
-    elif time_of_year == "Spring":
+    elif time_of_year == "spring":
       model_spring = joblib.load("ModelSpring")
       pred = model_spring.predict(listing)
-    elif time_of_year == "Summer":
+    elif time_of_year == "summer":
       model_summer = joblib.load("ModelSummer")
       pred = model_summer.predict(listing)
-    elif time_of_year == "Fall":
+    elif time_of_year == "fall":
       model_fall = joblib.load("ModelFall")
       pred = model_fall.predict(listing)
 
